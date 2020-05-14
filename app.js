b5913100aeb83e643667101930990aaac981b981
neggas <arsene.fop@gmail.com>
2020-05-14 16:48:33 +0000

    WebRtc dans le meme naviguateur

--- app.js
+++ app.js
@@ -5,17 +5,34 @@ const http = require('http');
 
 
 const app = express();
-express.static(".");
+
 const server = http.createServer(app);
 const io = socketIo(server);
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
-server.get('/', (res, res) => {
-    res.end('salut');
+app.use(express.static("."));
+
+//echange entre client et serer
+
+io.on('connection', (socket) => {
+    console.log("a user connected");
+
+    socket.on("disconnect", () => {
+        console.log("a user is disconect")
+    })
+
+    socket.on('signal', (data) => {
+        socket.emit('signalPeer', data);
+    })
 })
 
-const port = process.env.PORT || 3000;
 
-server.listen((port) => {
-    console.log("le serveur est ouvert sur le port " + port)
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("le serveur est ouvert sur le port " + PORT)
 })
--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-const socket = io();
-
-console.log(socket);
 window.onload = () => {
+
+    let peerEvent = (p) => {
+        p.on('signal', (data) => {
+            socket.emit("signal", data);
+        })
+    }
+
+    function gotMedia(stream) {
+        var peer1 = new SimplePeer({
+            initiator: true,
+            stream: stream
+        })
+        var peer2 = new SimplePeer()
+
+        peer1.on('signal', data => {
+            peer2.signal(data)
+        })
+
+        peer2.on('signal', data => {
+            peer1.signal(data)
+        })
+
+        peer2.on('stream', stream => {
+            // got remote video stream, now let's show it in a video tag
+            const videoPlayer = document.querySelector('#remote')
+
+            videoPlayer.srcObject = stream;
+
+            videoPlayer.play()
+        })
+
+
+    }
+
 
     let videoPlayer = document.getElementById('camera');
     navigator.mediaDevices.getUserMedia({
@@ -11,9 +42,11 @@ window.onload = () => {
         .then(stream => {
 
             videoPlayer.srcObject = stream;
+            gotMedia(stream)
+
         })
 
-    .catch(err => {
-        console.log(err);
-    })
+        .catch(err => {
+            console.log(err);
+        })
 }
